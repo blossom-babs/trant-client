9643c10b86e83263acdb9d7066c8eaf349fe0be7
Blossom <saphashb@gmail.com>
2022-06-12 01:04:55 +0100

    create top nav bar

---
+++ src/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+      <h1 className="navbar-container title">Tech Trant</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
