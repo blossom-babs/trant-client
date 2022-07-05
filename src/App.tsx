f895abcfc4bbad271e5e7bf788e3a9ed3738d7dc
Blossom <saphashb@gmail.com>
2022-07-05 04:27:01 +0100

    install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import {Navbar} from './components';
-import {Home, Login, Register, Settings, Single, Write} from './pages';
+import { Navbar } from './components';
+import { Home, Login, Register, Settings, Single, Write } from './pages';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-   <>
-   <Navbar/>
-   <Login/>
-   </>
-  );
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/settings" element={<Settings />} />
+				<Route path="/post/:postId" element={<Single />} />
+				<Route path="/post/write" element={<Write />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
