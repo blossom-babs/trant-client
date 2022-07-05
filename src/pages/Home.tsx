a78f5f00541a30675144d32ced01e445232a0c03
Blossom <saphashb@gmail.com>
2022-07-05 04:42:18 +0100

    update searchbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,18 @@
 import { Posts, Sidebar } from '../components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 	return (
 		<div className="home">
-			<Sidebar />
-			<Posts />
+			<div className="home__searchbar">
+				<input type="text" name="" id="" placeholder="Press l to search" />
+				<FontAwesomeIcon icon={faMagnifyingGlass} />
+			</div>
+			<div className="home__inner">
+				<Sidebar />
+				<Posts />
+			</div>
 		</div>
 	);
 };
