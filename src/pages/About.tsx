bb054d837cb6d585d625a44f73d24421e961c930
Blossom <saphashb@gmail.com>
2022-08-18 13:08:12 +0100

    about page content and styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
+import Logo from '../assets/trant-logo.png';
+import Blossom from '../assets/me.png';
+import {
+	faLinkedin,
+	faInstagram,
+	faYoutube,
+	faGithub,
+	faTiktok
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
 	return (
-		<div>
-			<h1>Trant</h1>
-			<p>
-				Trant is a <em>portmanteau, </em> coined from two words Tech and Rant.
-				Trant is a developer's corner on the internet for sharing her journey in
-				tech, career experiences, learnings and giving back to the ecosystem.
-				Hi, my name is Blossom and I trant ([rant, yap, chat, talk] about tech)!
-			</p>
-			<h1> Contact </h1>
-			<p>
-				If you want to say hello! Best way to reach me is on social media 👋{' '}
-			</p>
-      <div>
-        <p>icons in a box</p>
-      </div>
-      <hr/>
-		<p>	For all business inquiries, please email me 🤝</p>
-    <p> blossombabalola@gmail.com</p>
-			Kit HOME TIDBITS BLOG COURSES NEWSLETTER ABOUT
+		<div className="about">
+			<div>
+				<div className="about-trant">
+					<h1>About Trant</h1>
+					<div>
+						<img src={Logo} alt="Trant Brand" />
+						<p>
+							Trant is a <em>portmanteau, </em> coined from two words Tech and
+							Rant. Trant is a public documentation of a developer journey in
+							tech - career, fails, wins, community, public learnings and giving
+							back to the ecosystem.
+						</p>
+					</div>
+				</div>
+				<div className="about-me">
+					<h1>The Author</h1>
+					<div>
+						<img src={Blossom} alt="Blossom" />
+						<p>
+							Hi, my name is Blossom. I am a software developer based out of
+							Ibadan, Nigeria. I write python, full stack Javascript and
+							typsecript. My core strength is in building web applications that
+							have a seamless user experience with emphasis on web
+							accessibility, responsiveness and security. I love to learn,
+							travel, and immerse myself in different cultures. I am currently
+							studying <a>data structures and algorithms</a>, building trant and{' '}
+							<a>my ow software</a>. You can find me in the pages of a book or
+							any of my social media accounts:
+						</p>
+					</div>
+
+					<div>
+						<a
+							href="https://twitter.com/BlossomBabalola"
+							target="_blank"
+							rel="noreferrer">
+							<FontAwesomeIcon icon={faLinkedin} />
+						</a>
+						<a
+							href="https://anchor.fm/techtalk-with-blossom"
+							target="_blank"
+							rel="noreferrer">
+							<FontAwesomeIcon icon={faGithub} />
+						</a>
+						<a
+							href="https://anchor.fm/techtalk-with-blossom"
+							target="_blank"
+							rel="noreferrer">
+							<FontAwesomeIcon icon={faMicrophone} />
+						</a>
+						<a
+							href="https://anchor.fm/techtalk-with-blossom"
+							target="_blank"
+							rel="noreferrer">
+							<FontAwesomeIcon icon={faYoutube} />
+						</a>
+						<a
+							href="https://twitter.com/BlossomBabalola"
+							target="_blank"
+							rel="noreferrer">
+							<FontAwesomeIcon icon={faInstagram} />
+						</a>
+						<a
+							href="https://twitter.com/BlossomBabalola"
+							target="_blank"
+							rel="noreferrer">
+							<FontAwesomeIcon icon={faTiktok} />
+						</a>
+					</div>
+				</div>
+
+				<p> For all business inquiries, please email me 🤝</p>
+				<p> blossombabalola@gmail.com</p>
+				{/* press kit */}
+			</div>
 		</div>
 	);
 };
